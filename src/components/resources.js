2db1bfe37524a34735fdff8474e41ccde45a3ebf
Cristian Muscalu <muscaiu@yahoo.com>
2017-09-18 16:17:26 +0300

    add router and showing App children

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default ()=>{
+    return(
+        <div>
+            Rcp Secret
+            <ul>
+                <li>1 Sup</li>
+                <li>2 sec</li>
+                <li>3 w00t</li>
+            </ul>
+        </div>
+    )
+} 
+
+
